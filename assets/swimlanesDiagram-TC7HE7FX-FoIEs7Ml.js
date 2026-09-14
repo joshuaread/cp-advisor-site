cd52fa694d7de46839015ffab925cf8ed0e3110a
joshuaread <joshua.gray.read@gmail.com>
2026-09-13 19:39:24 -0500

    Publish AI Hero dark design system restyle
    Near-black surfaces, gold CTAs, DM Sans + JetBrains Mono; demos unchanged.

---
+++ assets/swimlanesDiagram-TC7HE7FX-FoIEs7Ml.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-KWPJA3E3-CdiaMatS.js";import{_ as a}from"./mermaid.core-B7OZVXj7.js";import"./chunk-5VM5RSS4-DW1CxDYs.js";import"./chunk-XXDRQBXY-CW0-7Wv5.js";import"./chunk-WEXAMYUT-DFex1g4k.js";import"./chunk-GWA4HPMP-Ce_BKj6Q.js";import"./channel-BiB7ObUQ.js";import"./index-D42dbaoG.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster:not([data-color-id]) rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,u=r({styles:s});export{u as diagram};
